c0bf2453ede50f849cd2c0ef0046a05a1b057379
hackerhgl <hackerhgl@gmail.com>
2020-08-20 19:26:09 -0700

    New maximize solution

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -12,27 +13,56 @@ import (
 	"github.com/go-vgo/robotgo"
 )
 
+type AppDimensions struct {
+	Width  int `json:width`
+	Height int `json:height`
+	X      int `json:x`
+	Y      int `json:y`
+}
+
 func main() {
 	start := time.Now()
 
 	var action string = strings.TrimSpace(os.Args[1])
 
 	if action == "maximize" {
+		if runtime.GOOS != "darwin" {
+			println("OS not supported")
+			return
+		}
 		var window string = strings.TrimSpace(os.Args[2])
+		var appID string = strings.TrimSpace(os.Args[3])
 		robotgo.ActiveName(window)
 
 		pid := robotgo.GetPID()
-		x, y, width, height := robotgo.GetBounds(pid)
-		println("x:", x)
-		println("y:", y)
-		println("width:", width)
-		println("height:", height)
+		robotgo.MaxWindow(pid)
 
-		offX := (width / 2) + x
-		offY := 15 + x
+		myself, _ := user.Current()
+		path := "/Users/" + myself.Username + "/Library/Containers/" + appID + "/Data/Documents/dimensions.json"
+		println(myself.Username)
+		println(path)
 
-		robotgo.Move(offX, offY)
+		rawDimensions, err := ioutil.ReadFile(path)
 
+		println(rawDimensions)
+
+		var dimensions AppDimensions
+
+		err = json.Unmarshal(rawDimensions, &dimensions)
+
+		if err != nil {
+			println("UNMARSAHLL ERROR")
+			println(err.Error())
+		}
+		println("dimensions [WHXY]", dimensions.Width, dimensions.Height, dimensions.X, dimensions.Y)
+		mousex, mousey := robotgo.GetMousePos()
+		println("PRE mousex", mousex, "mousey", mousey)
+		time.Sleep(time.Second)
+		robotgo.MoveMouseSmooth(dimensions.X+100, dimensions.Y+40)
+		mouse1x, mouse1y := robotgo.GetMousePos()
+		println("post mousex", mouse1x, "mousey", mouse1y)
+
+		time.Sleep(time.Second)
 		robotgo.MouseClick("left", true)
 
 		// println("PID", pid)
